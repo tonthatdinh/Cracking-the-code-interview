845683b141deec260792505fc9dd20a8c75a3bf1
tamdinh <dinh19197@gmail.com>
2019-08-31 22:32:22 +0700

    Add files via upload

---
+++ CheckPermutation.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * Problem: Given two strings, write a method to decide if one is a permutation
+ * of other.
+ *
+ * Solution: clarify case sensitive and whitespace
+ *
+ */
+
+public class CheckPermutation {
+    /**
+     * Solution Idea 2: Check if two strings have identical character counts
+     */
+
+    public boolean checkperm(String s1, String s2){
+        if (s1 == null || s2 == null || s1.length() != s2.length()){
+            return false;
+        }
+        int[] counts = new int[256];
+        for (char ch: s1.toCharArray()){
+            counts[ch]++;
+        }
+        for (char ch: s2.toCharArray()){
+            if (--counts[ch] < 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        CheckPermutation cp = new CheckPermutation();
+        String s1 = "ba";
+        String s2 = "ab";
+        System.out.println(cp.checkperm(s1, s2));
+    }
+}
---
+++ IsUnique.java
@@ -0,0 +1,22 @@
+/*
+Idea:
+1. get the first character of string s1
+2. iterator from the first to the end of s1 and check of s1[i] ?= s1[1]
+ */
+
+public class IsUnique {
+    boolean check(String s1) {
+        for(int i = 0; i < s1.length(); i ++){
+            if (s1.charAt(i) != s1.charAt(1))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        IsUnique isUnique = new IsUnique();
+        String s1 = "abcdedaaa";
+        System.out.println(s1);
+        System.out.println(isUnique.check(s1));
+    }
+}
